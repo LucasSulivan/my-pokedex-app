794b32fe48e2fe77600c9ce8ea13b5bc61676478
Lucas Sulivan <lucas.sulivan@hotmail.com>
2023-07-23 00:52:04 +0100

    pass te datas using props, moved the list of array pokemonList to App.jsx and pass te datas for PokemonCard using props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import PokemonCard from './Components/PokemonCard'
 import './App.css'
 
 function App() {
-  return (
-    <div>
-      <PokemonCard />
-    </div>
-  )
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  return <PokemonCard name={pokemonList[0].name} img={pokemonList[0].imgSrc}/>
+
+
 }
 
 export default App
--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,31 +1,24 @@
-function PokemonCard () {
-    const pokemon = pokemonList[1]
-    return (
-        <div>
-            {pokemon.name == "bulbasaur" ?
-            <figure>                
-                    <img src={pokemon.imgSrc} alt={pokemon.name}>
-                    </img>
-                    <figcaption>{pokemon.name}</figcaption>
-            </figure> 
-            :
-             <div>
-                <p> ??? </p>
-                <figcaption>{pokemon.name}</figcaption>
-            </div> }
-        </div>
+
+function PokemonCard ({name , img}) {
+    return  (       
+    <div>
+        {name == "bulbasaur" 
+        ?
+        <figure>                
+            <img src={img} alt={name}>
+            </img>
+            <figcaption>{name}</figcaption>
+        </figure> 
+        :
+     <div>
+        <p> ??? </p>
+        <figcaption>{name}</figcaption>
+    </div> }
+    </div>
     )
 }
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
+
 
 export default PokemonCard
